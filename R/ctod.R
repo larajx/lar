f2da1ab017d6d5966de5dc64a8bf23df2343dd73
larajx <larajx@gmail.com>
2022-04-11 11:49:20 -0400

    added the ctod function.  prints out a dataframe immidiately to the desktop.

---
+++ R/ctod.R
@@ -0,0 +1,15 @@
+#' Write out dataframe to the desktop
+#'
+#' @param df Name of object.  Can be anything like a dataframe, tibble, etc.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ctod <- function(df) {
+
+        path_name <- str_c("/Users/luisrodriguez/Desktop/", substitute(df), ".csv")
+        write.csv(df, path_name)
+}
+ctode(raw_df)
+
